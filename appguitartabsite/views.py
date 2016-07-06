51fb043863891c39c35a91c6fcf125ecf42c5b1e
KiaraRaynS <sherman.rayn@outlook.com>
2016-07-06 14:28:24 -0400

    Search Function implimented -Data response in progress

--- appguitartabsite/views.py
+++ appguitartabsite/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
 
-# Create your views here.
+
+def index_view(request):
+    # guitar site of choice: http://www.guitartabs.cc/
+    song_name = request.GET.get('song')
+    search_url = "http://www.guitartabs.cc/search.php?tabtype=any&band=&song={}".format(song_name)
+    results = requests.get(search_url).text
+    search_soup = BeautifulSoup(results, 'html.parser')
+    song_links = [link.attrs['href'] for link in search_soup.find_all('a', class_='ryzh22')]
+    print(song_links)
+    return render(request, 'index.html', {})
